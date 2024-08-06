2acb774e8f8b048078b83dfeaa6e2d3ef6f11af4
manurare <mreyarea@gmail.com>
2024-08-06 08:21:08 +0000

    rearrange

--- metrics.py
+++ metrics.py
@@ -29,8 +29,8 @@ gt_depth_files_format = os.path.join(args.gt_renders_dir, "{:05}_depth.dpt")
 
 metrics = {"psnr": [], "ssim": [], "lpips": [], "absrel": [], "rms": [], "delta1": [], "delta2": [], "delta3": []}
 
-with torch.no_grad():
-    for i in tqdm.tqdm(range(numfiles)):
+for i in tqdm.tqdm(range(numfiles)):
+    with torch.no_grad():
         pred_rgb = np.array(Image.open(pred_rgb_files_format.format(i))) / 255.
         gt_rgb = np.array(Image.open(gt_rgb_files_format.format(i))) / 255.
         pred_rgb = torch.from_numpy(pred_rgb).permute(2, 0, 1).type(torch.float32).contiguous().to("cuda")[None]
@@ -40,26 +40,26 @@ with torch.no_grad():
         ssim_ = ssim(pred_rgb, gt_rgb).cpu().item()
         lpips_ = lpips_alex(pred_rgb, gt_rgb).cpu().item()
 
-        # Depth
-        pred_depth = np.load(pred_depth_files_format.format(i))
-        gt_depth = read_dpt(gt_depth_files_format.format(i))
+    # Depth
+    pred_depth = np.load(pred_depth_files_format.format(i)).squeeze()
+    gt_depth = read_dpt(gt_depth_files_format.format(i)).suqeeze()
 
-        mask = gt_depth > 0
+    mask = gt_depth > 0
 
-        abs_rel = abs_rel_error(pred_depth, gt_depth, mask)
-        rms = lin_rms_sq_error(pred_depth, gt_depth, mask)
-        delta1 = delta_inlier_ratio(pred_depth, gt_depth, mask, 1)
-        delta2 = delta_inlier_ratio(pred_depth, gt_depth, mask, 2)
-        delta3 = delta_inlier_ratio(pred_depth, gt_depth, mask, 3)
+    abs_rel = abs_rel_error(pred_depth, gt_depth, mask)
+    rms = lin_rms_sq_error(pred_depth, gt_depth, mask)
+    delta1 = delta_inlier_ratio(pred_depth, gt_depth, mask, 1)
+    delta2 = delta_inlier_ratio(pred_depth, gt_depth, mask, 2)
+    delta3 = delta_inlier_ratio(pred_depth, gt_depth, mask, 3)
 
-        metrics["psnr"].append(psnr_)
-        metrics["ssim"].append(ssim_)
-        metrics["lpips"].append(lpips_)
-        metrics["absrel"].append(abs_rel)
-        metrics["rms"].append(rms)
-        metrics["delta1"].append(delta1)
-        metrics["delta2"].append(delta2)
-        metrics["delta3"].append(delta3)
+    metrics["psnr"].append(psnr_)
+    metrics["ssim"].append(ssim_)
+    metrics["lpips"].append(lpips_)
+    metrics["absrel"].append(abs_rel)
+    metrics["rms"].append(rms)
+    metrics["delta1"].append(delta1)
+    metrics["delta2"].append(delta2)
+    metrics["delta3"].append(delta3)
 
 means = {k:np.mean(v) for k,v in metrics.items()}
 print(",".join(["exp_name"] + [*means.keys()]))
