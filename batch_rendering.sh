c8c0a9776979436508d60a75c2c17a360615e8f3
manurare <mreyarea@gmail.com>
2024-08-08 15:41:31 +0000

    update batch processing

--- batch_rendering.sh
+++ batch_rendering.sh
@@ -7,8 +7,8 @@ for scene in apartment_0 apartment_1 apartment_2 frl_apartment_0 frl_apartment_1
     input_depth_path=example_data/replica/${scene}_lemniscate_1k_0/renders/input_depth.dpt
     expname=output/$scene
     
-    python core_exp_runner.py --config-name nerf_lemniscate dataset.image_path=$input_image_path dataset.depth_path=$input_depth_path device.base_exp_dir=$expname
-    python core_exp_runner.py --config-name nerf_lemniscate dataset.image_path=$input_image_path device.base_exp_dir=$expname mode=render_dense is_continue=true
+    python core_exp_runner.py --config-name nerf dataset.image_path=$input_image_path +dataset.depth_path=$input_depth_path device.base_exp_dir=$expname
+    python core_exp_runner.py --config-name nerf dataset.image_path=$input_image_path device.base_exp_dir=$expname mode=render_dense is_continue=true
 
     python metrics.py --exp_renders_dir ${expname}/WildDataset_renders/nerf_experiment/dense_images_new_persp/ --gt_renders_dir example_data/replica/${scene}_lemniscate_1k_0/renders
 done
